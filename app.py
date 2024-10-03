f2584c7f7a6b8c08b4e01a0b8d082917926c67a1
frozenhardliner <105587538+frozenhardliner@users.noreply.github.com>
2024-10-03 17:31:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,6 @@ footer {visibility: hidden;}
 
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 0.5rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
 # Helper function to load images as base64
 def get_image_as_base64(image_path):
     with open(image_path, "rb") as img_file:
